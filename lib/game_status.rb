c45c705d3cd894e4e17f644e2b812bb8204e571d
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-14 18:31:17 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -49,11 +49,7 @@ def draw?(board)
 end
 
 def over?(board)
-  if won?(board) || draw?(board) || full?(board)
-    return true
-  else
-    return false
-  end
+  won?(board) || draw?(board) || full?(board) ? true : false
 end
     
       
