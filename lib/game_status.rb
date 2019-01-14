26e767121e50907536781ec10313ec448b5620a5
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-14 17:16:34 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,15 +18,15 @@ WIN_COMBINATIONS = [
   ]
 
 def won?(board)
-each.WIN_COMBINATIONS do |win_combo|
-  each.win_combo do |win_index|
-    win_true = win_combo.all?{|index| board[index] = "X"} || win_combo.all?{|index| board[index] = "O"}
-    if(win_true) 
-      win_combo 
+  each.WIN_COMBINATIONS do |win_combo|
+    each.win_combo do |win_index|
+      win_true = win_combo.all?{|index| board[index] = "X"} || win_combo.all?{|index| board[index] = "O"}
+      if(win_true) 
+        win_combo 
+      end
     end
   end
-end
-return false
+  return false
 end
 
     
